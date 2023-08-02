deef8404c358472a0264143730084b13743aefb0
MV4Tech <mariyan102@abv.bg>
2023-08-02 15:21:32 +0300

    test Repository User save

--- src/main/java/com/example/todolist/task/model/Task.java
+++ src/main/java/com/example/todolist/task/model/Task.java
@@ -2,6 +2,7 @@ package com.example.todolist.task.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "TASK")
 public class Task {
