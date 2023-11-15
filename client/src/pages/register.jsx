ecf01b3269469b7962a2a38d726f4bc77640640a
Nikola104 <asd@abv.bg>
2023-11-15 20:49:19 +0200

    Register and refresh token repair

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,50 +1,96 @@
 import React from 'react'
 import '../styles/register.css'
+import { useState } from 'react'
 
 import Choveka from '../assets/images/login_image.jpg'
+import { useNavigate } from 'react-router-dom'
+import authService from '../services/auth-service'
 
 const Register = () => {
+
+    const [user, setUser] = useState({
+        username:"",
+        password:"",
+        email:"",
+        role:"USER"
+    })
+    const [confirmPassword, setConfirmPassword,] = useState("")
+    const [passwordError, setPasswordError] = useState("");
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setUser({...user,[e.target.name]: value})
+    }
+
+    const navigate = useNavigate();
+
+    const saveUser = async (e) => {
+        e.preventDefault();
+        if(confirmPassword == user.password){
+           await authService.makeRegisterRequest(user)
+            .then((response) => {
+                console.log(response);
+                navigate("/login")
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+            // Passwords don't match
+            setPasswordError('Passwords do not match');
+    }
+
+
+
   return (
-    <div class="css-grid">
-        <div class="header-text">
-            <h1 class="sign">    </h1>
+    <div className="css-grid">
+        <div className="header-text">
+            <h1 className="sign">    </h1>
         </div>
-        <div class="login-menu">
-            <div class="image-register-grid">
-                <div class="image-container">
-                    <img class="login_image" src={Choveka} alt="Choveka" />
+        <div className="login-menu">
+            <div className="image-register-grid">
+                <div className="image-container">
+                    <img className="login_image" src={Choveka} alt="Choveka" />
                 </div>
-                <div class="actual-login">
-                    <form>
-                        <div class="inputs">
-                            <div class="input-container">
-                                <label class="username-label" for="username">Username</label>
-                                <input type="text" id="username" name="username" placeholder="Enter your username" required />
+                <div className="actual-login">
+                    <form onSubmit={saveUser}>
+                        <div className="inputs">
+                            <div className="input-container">
+                                <label className="username-label" htmlFor="username">Username</label>
+                                <input type="text"
+                                 id="username" name="username" value={user.username} onChange={(e)=> handleChange(e)} placeholder="Enter your username" required />
                             </div>
-                            <div class="input-container">
-                                <label for="email">Email</label>
-                                <input type="email" id="email" name="email" placeholder="Enter your email" required />
+                            <div className="input-container">
+                                <label htmlFor="email">Email</label>
+                                <input type="email" id="email"
+                                 name="email" value={user.email} onChange={(e) => handleChange(e)} placeholder="Enter your email" required />
                             </div>
-                            <div class="input-container">
-                                <label for="password">Password</label>
-                                <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                            <div className="input-container">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" id="password"
+                                 name="password" value={user.password} onChange={(e) => handleChange(e)} placeholder="Enter your password" required />
                             </div>
-                            <div class="input-container">
-                                <label for="confirm_password">Confirm Password</label>
-                                <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm your password" required />
+                            <div className="input-container">
+                                <label htmlFor="confirm_password">Confirm Password</label>
+                                <input type="password"
+                                 id="confirm_password" 
+                                 name="confirmPassword"
+                                 value={confirmPassword}
+                                 onChange={(e)=> setConfirmPassword(e.currentTarget.value)}
+                                 placeholder="Confirm your password" required />
                             </div>
-                            <div class="input-container">
-                                <a class="sign-in-link" href="signin.html">You already have an account? Sign in here</a>
+                            <div className="input-container">
+                                <a className="sign-in-link" onClick={() => navigate("/login")} >You already have an account? Sign in here</a>
                             </div>
-                            <div class="input-container">
+                            <div className="input-container">
                                 <input type="submit" value="Sign Up" />
                             </div>
                         </div>
                     </form>
+                    {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
                 </div>
             </div>
         </div>
-        <div class="end">
+        <div className="end">
             
         </div>
     </div>
